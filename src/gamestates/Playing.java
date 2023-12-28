e0c326498c76123f8b62522f0cc1c0df35c63deb
Hogan <hoganwong88@gmail.com>
2023-12-28 16:02:02 +0800

    beginning to change level stuff

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -8,12 +8,20 @@ import entities.Player;
 import levels.LevelManager;
 import main.java.com.example.*;
 import ui.PauseOverlay;
+import utilz.LoadSave;
 
 public class Playing extends State implements Statemethods {
 	private Player player;
 	private LevelManager levelManager;
 	private PauseOverlay pauseOverlay;
 	private boolean paused=false; //Shows the pause screen, but set to false so that it doesn't appear immediately
+
+	private int xLvlOffset;
+	private int leftBorder= (int) (0.2*Game.GAME_WIDTH);
+	private int rightBorder=  (int) (0.8*Game.GAME_WIDTH); //If these need to be modified just in case, the 2 numbers need to add up to 1.
+	private int lvlTilesWide= LoadSave.GetLevelData()[0].length;
+	private int maxTilesOffset= lvlTilesWide-Game.TILES_IN_WIDTH;
+	private int maxLvlOffsetX= maxTilesOffset*Game.TILES_SIZE;
 
 	public Playing(Game game) {
 		super(game);
@@ -32,8 +40,27 @@ public class Playing extends State implements Statemethods {
 		if (!paused) {
 			levelManager.update();
 			player.update();
+			checkCloseToBorder();
 		} else {
 			pauseOverlay.update();
+		}
+	}
+
+	private void checkCloseToBorder() {
+		int playerX= (int) player.getHitBox().x;
+		int diff= playerX-xLvlOffset;
+
+		//Checks close to border
+		if (diff> rightBorder) {
+			xLvlOffset +=diff-rightBorder;
+		} else if (diff< leftBorder) {
+			xLvlOffset +=diff-leftBorder;
+		}
+
+		if (xLvlOffset> maxLvlOffsetX) {
+			xLvlOffset= maxLvlOffsetX;
+		} else if (xLvlOffset <0) {
+			xLvlOffset=0;
 		}
 	}
 
