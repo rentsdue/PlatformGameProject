79e3da44d2ab74c791bedf7c26a342ff01ea8435
Hogan <hoganwong88@gmail.com>
2024-05-20 09:45:18 +0800

    e

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -110,7 +110,7 @@ public class LoadSave {
     //     return imgs;
     // }
 
-     public static BufferedImage[] GetAllLevels() {
+    public static BufferedImage[] GetAllLevels() {
         List<String> levelNames = Arrays.asList("1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png");
         BufferedImage[] levels = new BufferedImage[levelNames.size()];
 
